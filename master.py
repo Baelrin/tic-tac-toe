a347dd229d40892c3cbc63f2dadf5c5f1de677a0
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-03 22:31:12 +0300

    Improve overral perfomance

--- master.py
+++ master.py
@@ -1,67 +1,79 @@
-# Number of cells
+# Define the number of cells for the game board
 board_size = 3
-# Game field
+
+# Define the game field
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Function to draw a single row of the board
+
+
+def draw_row(row):
+    return " " + str(row[0]) + " | " + str(row[1]) + " | " + str(row[2]) + "\n"
+
+# Function to display the game board
 
 
 def draw_board():
-    """Display game board"""
-    print("_" * 4 * board_size)
+    board_str = "_" * 4 * board_size + "\n"
+    # For each row in the board
     for i in range(board_size):
-        print((" " * 3 + "|") * 3)
-        print("", board[i * 3], "|", board[1 + i * 3],
-              "|", board[2 + i * 3], "|")
-        print(("_" * 3 + "|") * 3)
-    pass
+        row = [board[i * 3], board[1 + i * 3], board[2 + i * 3]]
+        board_str += " " * 3 + "|" + " " * 3 + "|" + " " * 3 + "\n"
+        board_str += draw_row(row)
+        board_str += "_" * 4 * board_size + "\n"
+    print(board_str)
+
+# Function to perform a move
 
 
 def game_step(index, char):
-    """Perform a move"""
-    if (index > 9 or index < 1 or board[index - 1] in ('X', 'O')):
+    try:
+        index = int(index)
+    except ValueError:
+        print("Invalid input. Please enter a number.")
         return False
+
+    # Check if the move is valid
+    if index < 1 or index > 9 or board[index - 1] in ('X', 'O'):
+        return False
+    # Set the new value at the index
     board[index - 1] = char
     return True
 
+# Function to check for victory of one of the players
+
 
 def check_win():
-    """Checking for victory of one of the players"""
-    win = False
-    win_combination = (
-        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
-        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
-        (0, 4, 8), (2, 4, 6)  # Diagonal
-    )
+    win_combinations = {(0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+                        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+                        (0, 4, 8), (2, 4, 6)}  # Diagonal
 
-    for pos in win_combination:
-        if (board[pos[0]] == board[pos[1]] and board[pos[1]] == board[pos[2]]):
-            win = board[pos[0]]
-    return win
+    for combination in win_combinations:
+        if board[combination[0]] == board[combination[1]] == board[combination[2]] != ' ':
+            return board[combination[0]]
+    return None
+
+# Function to start the game
 
 
 def start_game():
-    # Current Player
-    current_player = "X"
-    # Other Player
-    other_player = "O"
-    # Move Number
-    move_number = 1
+    current_player = "X"  # The current player
+    move_number = 1  # The current move number
     draw_board()
 
+    # Loop until there are no more moves
     while move_number < 10:
-        index = input(
-            "Current player => "
-            + current_player
-            + ". Enter the number (0 - exit):"
-        )
+        index = input("Current player => " + current_player +
+                      ". Enter the number (0 - exit):")
         if int(index) == 0:
             break
-        elif index.isdigit():
-            # If move was successful
+        elif 1 <= int(index) <= 9:
+            # If the move was successful
             if game_step(int(index), current_player):
                 print('Successful move')
                 draw_board()
                 # Switch players
-                current_player, other_player = other_player, current_player
+                current_player = "O" if current_player == "X" else "X"
                 move_number += 1
                 # Check if there is a winner
                 winner = check_win()
