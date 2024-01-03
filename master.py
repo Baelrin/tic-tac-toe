051f14a70bc7175bdb39b190588a1417bcc99ad3
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-03 18:50:19 +0300

    Create basic structure

--- master.py
+++ master.py
@@ -1,1 +1,21 @@
+# Number of cells
+board_size = 3
+# Game field
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+def draw_board():
+  '''Display game board'''
+  pass
+
+def game_step():
+  '''Perform a move'''
+  pass
+
+def check_win():
+  pass
+
+def start_game():
+  draw_board()
+
+print('Добро пожаловать в Крестики-нолики!')
+start_game()
