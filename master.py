3e40b29183c784ca37a2596194072ced875a823b
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-04 22:46:50 +0300

    Some more optimizations
    Improve draw_board, check_win, game_step, computer_step

--- master.py
+++ master.py
@@ -13,11 +13,13 @@ def draw_board():
     Draw the game board:
     This function provides a visual representation of the game board.
     '''
-    print(('_' * 4 * board_size))
+    print('_' * 4 * board_size)
     for i in range(board_size):
-        print((' ' * 3 + '|') * 3)
-        print('', board[i*3], '|', board[1+i*3], '|', board[2+i*3], '|')
-        print(('_' * 3 + '|') * 3)
+        cell1 = board[i*3]
+        cell2 = board[1+i*3]
+        cell3 = board[2+i*3]
+        print(f' {cell1} | {cell2} | {cell3} ')
+        print('_' * 4 * board_size)
 
 
 def check_win(board):
@@ -25,16 +27,16 @@ def check_win(board):
     Check the game status:
     This function checks whether any player (X or O) has already won the game.
     '''
-    win = False
     win_combination = (
         (0, 1, 2), (3, 4, 5), (6, 7, 8),  # horizontal lines
         (0, 3, 6), (1, 4, 7), (2, 5, 8),  # vertical lines
-        (0, 4, 8), (2, 4, 6)              # diagonal lines
+        (0, 4, 8), (2, 4, 6)             # diagonal lines
     )
-    for pos in win_combination:
-        if (board[pos[0]] == board[pos[1]] and board[pos[1]] == board[pos[2]] and board[pos[1]] in ('X', 'O')):
-            win = board[pos[0]]
-    return win
+    win_set = set(win_combination)
+    for pos in win_set:
+        if (board[pos[0]] == board[pos[1]] == board[pos[2]]) and board[pos[1]] in ('X', 'O'):
+            return board[pos[0]]
+    return False
 
 
 def game_step(index, char):
@@ -42,7 +44,9 @@ def game_step(index, char):
     Make a player's move:
     This function updates the game board with the new move.
     '''
-    if (index > 10 or index < 1 or board[index-1] in ('X', 'O')):
+    if index < 1 or index > 9:
+        return False
+    if board[index-1] in ('X', 'O'):
         return False
     board[index-1] = char
     return True
@@ -53,14 +57,16 @@ def computer_step(human, ai):
     Make the AI's move:
     This function calculates the next move for the AI, based on the current board state.
     '''
-    available_steps = [i-1 for i in board if type(i) == int]
+    available_steps = {i-1 for i in board if type(i) == int}
     win_steps = (4, 0, 2, 6, 8, 1, 3, 5, 7)
     for char in (ai, human):
         for pos in available_steps:
-            board_ai = board[:]
-            board_ai[pos] = char
-            if (check_win(board_ai) != False):
+            temp = board[pos]
+            board[pos] = char
+            if (check_win(board) != False):
+                board[pos] = temp
                 return pos
+            board[pos] = temp
     for pos in win_steps:
         if (pos in available_steps):
             return pos
@@ -106,7 +112,7 @@ def start_game(mode):
     if (step > 8):
         print('The game is over. Draw!')
     elif check_win(board):
-        print('Won ' + check_win(board))
+        print('Won ' + str(check_win(board)))
 
 
 # Begin game entrance point
